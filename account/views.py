7817fa8e66f851966999a35f34cf9484a66d89c1
Vikt0r <vikt0r.nekrut@outlook.com>
2019-05-05 01:48:04 +0300

    auth with email added

--- account/views.py
+++ account/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
@@ -41,6 +42,10 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, "Profile updated successfully")
+
+        else:
+            messages.error(request, 'Error updating your profile')
 
     else:
         user_form = UserEditForm(instance=request.user)
